6fb99bd12328f36c0637fcc8cddd2f1496fd3be0
Bibhavcodeverse <bibhavkumar05@gmail.com>
2024-11-24 17:45:19 +0530

    commit

--- practice.py
+++ practice.py
@@ -2,10 +2,25 @@ import time
 
 t=time.strftime('%H:%M:%S')
 
+
 hour =int(time.strftime("%H"))
 
+mintues= int(time.strftime("%M"))
+
+second= int(time.strftime("%S"))
+
 hour=int(input("Enter hour: "))
+mintues=int(input("Enter mintues: "))
+second=int(input("Enter second: "))
+
+
 print(hour)
+print(mintues)
+print(second)
+
+
+
+
 
 if (hour>=0 and hour<12):
     print("Good Morning Sir!")
@@ -15,3 +30,5 @@ elif (hour>=12 and hour<17):
 
 elif (hour>=17 and hour<0):
     print("Good night Sir!")
+
+
